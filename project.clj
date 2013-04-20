ed310deab030f65a26fb8b6d78283c7656537d84
Zhe Wu <wu@madk.org>
2013-04-20 13:13:51 +0800

    add url to projects.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject clj-redis-session "1.0.0"
+  :url "https://github.com/wuzhe/clj-redis-session"
   :description "Redis-backed Clojure/Ring session store"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [ring/ring-core "1.2.0-beta2"]
