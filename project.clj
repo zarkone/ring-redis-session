ec5b76057f48e0ba4a90938dd66da835b3db1d7a
Zhe Wu <wu@madk.org>
2014-04-20 09:24:35 +0800

    fix ring-core version

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :url "https://github.com/wuzhe/clj-redis-session"
   :description "Redis-backed Clojure/Ring session store"
   :dependencies [[org.clojure/clojure "1.5.0"]
-                 [ring/ring-core "1.3.0"]
+                 [ring/ring-core "1.2.2"]
                  [com.taoensso/carmine "2.6.0"]])
