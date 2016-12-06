130ce037e65c29ef8f71a73bd0485276fc8a0b3a
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2016-12-06 11:04:01 -0600

    Updated to use latest stable Ring release.

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :url "https://github.com/wuzhe/clj-redis-session"
   :description "Redis-backed Clojure/Ring session store"
   :dependencies [[org.clojure/clojure "1.5.0"]
-                 [ring/ring-core "1.2.2"]
+                 [ring/ring-core "1.5.0"]
                  [com.taoensso/carmine "2.15.0"]])
