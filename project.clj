45a796084bd7df1ee6822c39eb28ac003cb25e3b
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2016-12-06 11:31:43 -0600

    Added support for Travis CI build matrix.

--- project.clj
+++ project.clj
@@ -3,4 +3,21 @@
   :description "Redis-backed Clojure/Ring session store"
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [ring/ring-core "1.6.0-beta6"]
-                 [com.taoensso/carmine "2.15.0"]])
+                 [com.taoensso/carmine "2.15.0"]]
+  :profiles {
+    :uber {
+      :aot :all}
+    :1.5 {
+      :dependencies [
+        [org.clojure/clojure "1.5.0"]
+        [medley "0.6.0" :exclusions [org.clojure/clojure]]]}
+    :1.6 {
+      :dependencies [
+        [org.clojure/clojure "1.6.0"]
+        [medley "0.6.0" :exclusions [org.clojure/clojure]]]}
+    :1.7 {
+      :dependencies [[org.clojure/clojure "1.7.0"]]}
+    :1.8 {
+      :dependencies [[org.clojure/clojure "1.8.0"]]}
+    :1.9 {
+      :dependencies [[org.clojure/clojure "1.9.0-alpha14"]]}})
