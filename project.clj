8c13a0856c10e841e3682da137040c0145765ec5
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2016-12-13 18:50:59 -0600

    Added logging (for debug purposes).

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject clojusc/clj-redis-session "2.1.4-SNAPSHOT"
   :url "https://github.com/wuzhe/clj-redis-session"
   :description "Redis-backed Clojure/Ring session store"
-  :dependencies [[org.clojure/clojure "1.5.0"]
-                 [ring/ring-core "1.6.0-beta6"]
-                 [com.taoensso/carmine "2.15.0"]]
+  :dependencies [[com.taoensso/carmine "2.15.0"]
+                 [org.clojure/clojure "1.5.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [ring/ring-core "1.6.0-beta6"]]
   :profiles {
     :uber {
       :aot :all}
--- src/clj_redis_session/core.clj
+++ src/clj_redis_session/core.clj
@@ -1,7 +1,8 @@
 (ns clj-redis-session.core
   "Redis session storage."
   (:use ring.middleware.session.store)
-  (:require [taoensso.carmine :as car :refer [wcar]])
+  (:require [clojure.tools.logging :as log]
+            [taoensso.carmine :as car :refer [wcar]])
   (:import java.util.UUID))
 
 (defn new-session-key [prefix]
@@ -11,6 +12,7 @@
   SessionStore
   (read-session [_ session-key]
     (when session-key
+      (log/debug "In read-session, 'session-key':" session-key)
       (when-let [data (wcar redis-conn (car/get session-key))]
         (if (and expiration reset-on-read)
           (wcar redis-conn (car/expire session-key expiration)))
@@ -19,6 +21,8 @@
     (let [session-key (or session-key (new-session-key prefix))
           data-str (binding [*print-dup* true]
                      (print-str data))]
+      (log/debug "In write-session, 'session-key':" session-key)
+      (log/debug "In write-session, 'data-str':" data-str)
       (if expiration
         (wcar redis-conn (car/setex session-key expiration data-str))
         (wcar redis-conn (car/set session-key data-str)))
@@ -34,4 +38,5 @@
   ([redis-conn {:keys [prefix expire-secs reset-on-read]
                 :or {prefix "session"
                      reset-on-read false}}]
+     (log/debug "Creating Redis store ...")
      (RedisStore. redis-conn prefix expire-secs reset-on-read)))
