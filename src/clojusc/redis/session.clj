edc047de6858bd93267b6ff6ad284412d3f6f01c
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2016-12-13 19:57:50 -0600

    Removed `use` in favour of `require`.

--- src/clojusc/redis/session.clj
+++ src/clojusc/redis/session.clj
@@ -1,7 +1,7 @@
 (ns clojusc.redis.session
   "Redis session storage."
-  (:use ring.middleware.session.store)
   (:require [clojure.tools.logging :as log]
+            [ring.middleware.session.store :as api]
             [taoensso.carmine :as car :refer [wcar]])
   (:import java.util.UUID))
 
@@ -9,7 +9,7 @@
   (str prefix ":" (str (UUID/randomUUID))))
 
 (deftype RedisStore [redis-conn prefix expiration reset-on-read]
-  SessionStore
+  api/SessionStore
   (read-session [_ session-key]
     (when session-key
       (log/debug "In read-session, 'session-key':" session-key)
@@ -21,8 +21,8 @@
     (let [session-key (or session-key (new-session-key prefix))
           data-str (binding [*print-dup* true]
                      (print-str data))]
-      (log/debug "In write-session, 'session-key':" session-key)
-      (log/debug "In write-session, 'data-str':" data-str)
+      (log/debug "In write-session (v3), 'session-key':" session-key)
+      (log/debug "In write-session (v3), 'data-str':" data-str)
       (if expiration
         (wcar redis-conn (car/setex session-key expiration data-str))
         (wcar redis-conn (car/set session-key data-str)))
