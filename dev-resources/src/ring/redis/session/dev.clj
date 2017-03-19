ff5dc7566f500ad93b387f0aea5f4f8c5c1daea3
Duncan McGreggor <oubiwann@gmail.com>
2017-03-19 10:32:51 -0500

    Added dev namespace.

---
+++ dev-resources/src/ring/redis/session/dev.clj
@@ -0,0 +1,41 @@
+(ns ring.redis.session.dev
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.pprint :refer [pprint print-table]]
+            [clojure.reflect :refer [reflect]]
+            [clojure.string :as string]
+            [clojure.walk :refer [macroexpand-all]]
+            [ring.redis.session :refer [redis-store]]
+            [taoensso.carmine :as car :refer [wcar]]))
+
+(defn make-session-store
+  ([]
+    (make-session-store
+      {:pool {}
+       :spec {:host "127.0.0.1"
+              :port 6379}}))
+  ([conn]
+    (make-session-store
+      conn
+      {:expire-secs 43200
+       :reset-on-read true}))
+  ([conn opts]
+    (redis-store conn opts)))
+
+(def default-connection (make-session-store))
+
+(defmacro redis
+  "With this macro we can do things like the following in the REPL (for
+  querying Redis):
+
+  ```clj
+  => (redis 'ping)
+  => (redis 'get \"testkey\")
+  => (redis 'set \"foo\" \"bar\")
+  ```
+
+  (Note that the escaped strings are for the docstring, and not what you'd
+  actually type in the REPL.)"
+  [cmd-str & body]
+  `(car/wcar default-connection
+             ((resolve (symbol (str "car/" ~cmd-str))) ~@body)))
